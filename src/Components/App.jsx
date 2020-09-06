ae69cfe715b908b465711a4b1a4f65f87c1b69ee
vikramoff24 <vikram.senthil@outlook.com>
2020-09-06 18:51:31 +0530

    Added Props

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../Notes"
 function App()
 {
     return <div><Header/>
-    <Note/>
+    <Note key={Notes[0].key} title={Notes[0].title} content={Notes[0].content}/>
     <Footer/>
     </div>
 }
