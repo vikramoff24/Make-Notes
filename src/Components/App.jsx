ed0cc4e58e117f19831b876b73bfd39676127c14
vikramoff24 <vikram.senthil@outlook.com>
2020-09-06 19:12:24 +0530

    Added Arrow Function

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,11 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import Notes from "../Notes"
+
 function App()
 {
-    return <div><Header/>
-    <Note key={Notes[0].key} title={Notes[0].title} content={Notes[0].content}/>
-    <Footer/>
+    return <div>
+
+    <Header/>
+   {Notes.map(note=>(<Note key={note.key}
+     title={note.title} 
+     content={note.content}/>))}
+         
+     <Footer/>
     </div>
 }
 export default App;
