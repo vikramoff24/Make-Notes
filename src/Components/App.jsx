5e51b83f1c75f17ab7e84d912c0764c219c1da3e
Vikram S <vikram.senthil@outlook.com>
2020-09-21 01:35:13 +0530

    Added Diff way to delete

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,32 +1,40 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 function App() {
-const [notesArr,addNotesArr]=useState([])
-function handleClick(notes)
-  {
-  addNotesArr(prevValue=>
-    {
-      return[...prevValue,
+  const [notesArr, addNotesArr] = useState([])
+  function handleClick(notes) {
+    addNotesArr(prevValue => {
+      return [...prevValue,
         notes
       ]
     })
   }
-  function deleteNotes(id)
-  {
-addNotesArr(notesArr.filter((notes,index)=>
-{
-return index!==id;
-}));
+
+  // Two Ways of implementing
+  // function deleteNotes(id) {
+  //   addNotesArr(notesArr.filter((notes, index) => {
+  //     return index !== id;
+  //   }));
+  // }
+
+  function deleteNotes(id) {
+    addNotesArr((prevValue)=> prevValue.filter((notes, index) => {
+      return index !== id;
+    }));
   }
+
+
+
+
   return (
     <div>
       <Header />
-      <CreateArea  onClicked={handleClick} />
-      {notesArr.map((notes,index)=>  <Note key={index} id= {index} title={notes.title} content={notes.content} onClicked={deleteNotes }/> )}
-    
+      <CreateArea onClicked={handleClick} />
+      {notesArr.map((notes, index) => <Note key={index} id={index} title={notes.title} content={notes.content} onClicked={deleteNotes} />)}
+
       <Footer />
     </div>
   );
