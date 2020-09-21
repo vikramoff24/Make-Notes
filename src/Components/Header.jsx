0723113ad8c209479e8357e36e637fbaac71b576
Vikram S <vikram.senthil@outlook.com>
2020-09-21 20:32:35 +0530

    Added header icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><WbIncandescentIcon fontSize="large" style={{paddingTop:15}}/>Keeper</h1>
     </header>
   );
 }
-
 export default Header;
