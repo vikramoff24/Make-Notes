b79fc4a23e2c3e65dc86e7ad02810dfc283eab85
Vikram S <vikram.senthil@outlook.com>
2020-09-21 20:04:22 +0530

    Added Conditional Rendering, In attribute

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,33 +1,46 @@
 import React,{useState} from "react";
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 function CreateArea(props) {
     const [notes,setNotes]=useState({
         title:"",
         content:""
       });
+      const [bool,setBool]=useState(false);
       function handleChange(event)
 {
 const {name, value }=event.target;
 setNotes((prevValue)=>{
 return {...prevValue,
-    [name]:value}
-})
+    [name]:value}})
+
+    if(name==="content")
+    {
+      setBool(true);
+    }
+}
+
+function handleClick()
+{
+
 }
 return (
  <div>
 <form autocomplete="off" className= "create-note">
-        <input name="title" placeholder="Title"  value={notes.title} onChange={handleChange}/>
-        <textarea name="content" placeholder="Take a note..." rows="3" value={notes.content} onChange={handleChange} />
-        <Fab onClick= {(event)=>
+        {bool&&<input name="title" placeholder="Title"  value={notes.title} onChange={handleChange}/>}
+        <textarea name="content" placeholder="Take a note..." rows={bool===false?"1":"3"} value={notes.content} onChange={handleChange} onClick={handleClick} />
+       <Zoom in = {!bool?false:true}><Fab onClick= {(event)=>
         {
             props.onClicked(notes);
             setNotes({
                 title:"",
                 content:"" 
-            });
+            }
+           );
+           setBool(false);
 event.preventDefault();
-}}><AddIcon/></Fab>
+}}><AddIcon/></Fab></Zoom> 
       </form>
     </div>
   );
